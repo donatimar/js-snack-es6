6c386c1cf15cbc4064cb8dd6c567f6493ca18e3f
donatimar <donatimar@gmail.com>
2024-10-25 15:40:28 +0200

    snack 3 - bonus

--- snack 3/js/main.js
+++ snack 3/js/main.js
@@ -2,3 +2,20 @@
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 */
+
+function getValues(array, a, b) {
+  if (a >= b) {
+    console.error("A deve essere minore di B");
+    return [];
+  }
+
+  const newArray = array.slice(a, b);
+  return newArray;
+}
+
+const a = parseInt(prompt("Inserisci il valore di A"), 10);
+const b = parseInt(prompt("Inserisci il valore di B"), 10);
+
+const myArray = [1, 2, 3, 4, 5];
+const result = getValues(myArray, a, b);
+console.log(result);
